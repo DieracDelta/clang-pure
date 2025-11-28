5c177e69795346c67d1adeb3d14ef1202f00fae9
Justin Restivo <justin@restivo.me>
2025-11-28 18:04:47 -0500

    feat: build with newer haskell

--- src/Language/C/Clang/Cursor.hs
+++ src/Language/C/Clang/Cursor.hs
@@ -14,43 +14,42 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 
-{-|
+{- |
 Module      :  Language.C.Clang.Cursor
 Copyright   :  (C) 2016 Patrick Chilton
 
 If you know what `CursorKind`s you want to operate on,
 consider using "Language.C.Clang.Cursor.Typed" instead.
 -}
-module Language.C.Clang.Cursor
-  ( Cursor()
-  , translationUnitCursor
-  , cursorTranslationUnit
-  , cursorChildrenF
-  , cursorChildren
-  , cursorDescendantsF
-  , cursorDescendants
-  , cursorSpelling
-  , cursorExtent
-  , cursorLocation
-  , cursorUSR
-  , cursorReferenced
-  , cursorType
-  , cursorKind
-
-  , TypeLayoutError(..)
-  , offsetOfField
-  , CursorKind(..)
-  )
+module Language.C.Clang.Cursor (
+    Cursor (),
+    translationUnitCursor,
+    cursorTranslationUnit,
+    cursorChildrenF,
+    cursorChildren,
+    cursorDescendantsF,
+    cursorDescendants,
+    cursorSpelling,
+    cursorExtent,
+    cursorLocation,
+    cursorUSR,
+    cursorReferenced,
+    cursorType,
+    cursorKind,
+    TypeLayoutError (..),
+    offsetOfField,
+    CursorKind (..),
+)
 where
 
 import Data.Functor.Contravariant
-import Lens.Micro
+import Lens.Micro hiding (cosmosOf)
 import Lens.Micro.Contra
 
 import Language.C.Clang.Internal.FFI
 import Language.C.Clang.Internal.Types
 
-cursorChildren :: Cursor -> [ Cursor ]
+cursorChildren :: Cursor -> [Cursor]
 cursorChildren = toListOf cursorChildrenF
 
 cosmosOf :: (Applicative f, Contravariant f) => LensLike' f a a -> LensLike' f a a
@@ -61,5 +60,5 @@ cursorDescendantsF :: Fold Cursor Cursor
 cursorDescendantsF = cosmosOf cursorChildrenF
 
 -- | List a `Cursor` and all of its descendants recursively.
-cursorDescendants :: Cursor -> [ Cursor ]
+cursorDescendants :: Cursor -> [Cursor]
 cursorDescendants = toListOf cursorDescendantsF
